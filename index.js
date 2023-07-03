26fc99e4efb0e649f6a6ecd2d5b404ee5b64d3cc
Caleb Kiprop Rono <ckiprop14@gmail.com>
2023-07-03 12:39:19 +0300

    Lab done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+let changeText = document.getElementById('text')
+document.addEventListener('DOMContentLoaded', function(){
+    changeText.innerHTML = "This is really cool!"
+})
+
+// })
+// changeText.innerHTML = "This is really cool!"
+// console.log(changeText)
+
+
+  
